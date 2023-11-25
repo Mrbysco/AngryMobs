0d5ce9842f2c451edb7fe7be358d4b964e085a7f
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-25 03:40:06 +0100

    Remove unused import

--- src/main/java/com/mrbysco/angrymobs/registry/tweaks/MeleeAttackTweak.java
+++ src/main/java/com/mrbysco/angrymobs/registry/tweaks/MeleeAttackTweak.java
@@ -6,7 +6,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.PanicGoal;
 import net.minecraftforge.registries.ForgeRegistries;
