01a48e43dc3df0af1ebbb63b586e0d223f97fcb0
DoodleSchrank <39834655+DoodleSchrank@users.noreply.github.com>
2025-03-06 12:28:33 +0100

    revert accidental change

--- src/main/java/com/mrbysco/angrymobs/registry/TweakTypeRegistry.java
+++ src/main/java/com/mrbysco/angrymobs/registry/TweakTypeRegistry.java
@@ -2,7 +2,6 @@ package com.mrbysco.angrymobs.registry;
 
 import com.mojang.serialization.MapCodec;
 import com.mrbysco.angrymobs.AngryMobs;
-import com.mrbysco.angrymobs.config.AngryConfig;
 import com.mrbysco.angrymobs.registry.condition.ConfigEnabledCondition;
 import com.mrbysco.angrymobs.tweaks.AttackNearestTweak;
 import com.mrbysco.angrymobs.tweaks.AvoidEntityTweak;
@@ -28,6 +27,7 @@ public class TweakTypeRegistry {
 		event.dataPackRegistry(LookAtEntityTweak.REGISTRY_KEY, LookAtEntityTweak.DIRECT_CODEC, LookAtEntityTweak.DIRECT_CODEC);
 		event.dataPackRegistry(MeleeAttackTweak.REGISTRY_KEY, MeleeAttackTweak.DIRECT_CODEC, MeleeAttackTweak.DIRECT_CODEC);
 		event.dataPackRegistry(ProjectileAttackTweak.REGISTRY_KEY, ProjectileAttackTweak.DIRECT_CODEC, ProjectileAttackTweak.DIRECT_CODEC);
+		
 		AngryMobs.LOGGER.info("Registered Tweak registries");
 	}
 
